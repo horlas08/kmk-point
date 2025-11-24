3511d19f22506ade92477ae5bf3a7f6b0a1adeb7
qozeem <qozeemmonsurudeen@gmail.com>
2025-11-24 23:47:16 +0100

    scan done

--- lib/app/modules/scan/views/scan_view.dart
+++ lib/app/modules/scan/views/scan_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:point_system/app/common/widgets/custom_button.dart';
+import 'package:point_system/app/common/widgets/space.dart';
 import 'package:point_system/app/constants/colors.dart';
 import 'package:qr_code_scanner_plus/qr_code_scanner_plus.dart';
 
@@ -29,7 +31,7 @@ class _ScanViewState extends State<ScanView> {
         child: Column(
           children: [
             Expanded(
-              flex: 3,
+              flex: 5,
               child: QRView(
                 key: controller.qrKey,
                 onQRViewCreated: controller.onQRViewCreated,
@@ -41,17 +43,24 @@ class _ScanViewState extends State<ScanView> {
               ),
             ),
             Expanded(
-              flex: 1,
-              child: Center(
-                child: Obx(() {
-                  return controller.result.value != null
-                      ? Text(
-                    'Barcode Type: ${controller.result.value!.format}\n'
-                        'Data: ${controller.result.value!.code}',
-                    textAlign: TextAlign.center,
-                  )
-                      : const Text('Scan a code');
-                }),
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  
+                  children: [
+                    Text(
+                      'scanning'.tr,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text("place_barcode".tr,),
+                    vSpace(24),
+                    CustomButton(text: "cancel".tr, onPressed: () => Get.back(),)
+                  ],
+                ),
               ),
             ),
           ],
